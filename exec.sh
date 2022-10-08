b897065c202306c6c86d871f6bc230bcd16bbdcb
aryaraj132 <aryaraj132@gmail.com>
2022-10-08 22:05:20 +0530

    docker control execution

---
+++ exec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo docker rm next_app_cont -f
+
+id=$(sudo docker images next_app -q)
+
+sudo docker build --rm -t next_app .
+
+sudo docker run -dp 3000:3000 --name next_app_cont next_app
+
+new_id=$(sudo docker images next_app -q)
+
+if [ $id == $new_id ]
+    then
+    sudo docker rmi $(sudo docker images --filter "dangling=true")
+    else
+    sudo docker image rm $id
+    sudo docker rmi $(sudo docker images --filter "dangling=true")
+fi
