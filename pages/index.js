6b391c30423c9c63f56140071b2b90422b0fb9b3
aryaraj132 <aryaraj132@gmail.com>
2022-10-10 21:33:18 +0530

    Landing Page Skipped

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,18 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { userState } from '../_recoil/userState';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 export default function Home() {
   const [user,setUser] = useRecoilState(userState);
-  
+  const router = useRouter();
+  useEffect(()=>{
+    if (user){
+      router.push('/dashboard')
+    }else {
+      router.push('/login')
+    }
+    },[])
   return (
     <div>
       <Head>
